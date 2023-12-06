3ab7d128db900f0c7ff203d735b802097ad07db7
nimitz-Bear <isaacdestura@gmail.com>
2023-12-06 21:04:58 +0000

    day 6: part 1

---
+++ day6/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
+    let input = fs::read_to_string("full.txt").expect("Should have been able to read a file");
+    let lines: Vec<_> = input.split('\n').collect();
+
+    let re = Regex::new(r"\d+").unwrap();
+    let times: Vec<_> = re.find_iter(lines[0]).map(|m| m.as_str().parse::<usize>().unwrap()).collect();
+    let records: Vec<_>  = re.find_iter(lines[1]).map(|m| m.as_str().parse::<usize>().unwrap()).collect();
+
+    let mut counts: Vec<u32> = Vec::new();
+
+    // go through all the races
+    for (i,time) in times.iter().enumerate() {
+        let mut count = 0;
+     
+        // go through all possible seconds from 1 to time - 1
+        for x in 1..*time {
+            let distance = x*(time - x);
+            if distance > records[i] {
+                count +=1;
+            }
+        }
+        println!("{}", count);
+        counts.push(count);
+    }
+
+    println!("Part 1: {}", counts.iter().fold(1, |product, x| product * x));
+
+}
+
+fn part_2() {
+    let input = fs::read_to_string("full.txt").expect("Should have been able to read a file");
+    let lines: Vec<_> = input.split('\n').collect();
+
+    let re = Regex::new(r"\d+").unwrap();
+    let times: Vec<_> = re.find_iter(lines[0]).map(|m| m.as_str().parse::<usize>().unwrap()).collect();
+    let records: Vec<_>  = re.find_iter(lines[1]).map(|m| m.as_str().parse::<usize>().unwrap()).collect();
+
+}
